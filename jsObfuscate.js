7eeb54c4179af85280a5e54c34a2823136de79fa
sergei_sergeev <sergeev.srg@gmail.com>
2015-11-18 22:09:46 +0300

    Fix for recent updates from javascriptobfuscator.com

--- jsObfuscate.js
+++ jsObfuscate.js
@@ -118,9 +118,12 @@ function obfuscate(strInput, options) {
   return request().
   then(function(res) {
     var headers = {};
-    var cookie = res.headers['set-cookie'][0];
-    var cookiedata = cookie.slice(0, cookie.indexOf(';'));
-    headers['Cookie'] = cookiedata;
+	if(res.headers['set-cookie']){
+		var cookie = res.headers['set-cookie'][0];
+		var cookiedata = cookie.slice(0, cookie.indexOf(';'));
+		headers['Cookie'] = cookiedata;
+	}
+    
     headers['Content-Type'] = 'application/x-www-form-urlencoded';
     return getFormData(res.body).then(function(formData) {
       return {
